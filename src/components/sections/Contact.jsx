1bd2f4f89cc26778d0d752c8eff5c7dd41e987c7
Your Name <you@example.com>
2025-05-20 23:46:56 +0500

    Update something

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -29,7 +29,7 @@ export const Contact=()=>{
             <div className="px-4 w-150">
                 <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
                {''} Get In Touch</h2>
-                <form onSubmit={handleSubmit} className="space-y-6 bg-sky-700 p-10 rounded-3xl">
+                <form onSubmit={handleSubmit} className="space-y-6 bg-sky-700 p-10 rounded-3xl w-[400px] mx-auto md:w-full">
                     <div className="relative">
                         <input type="text" id="name" name="name" required placeholder="Name..." value={formData.name} onChange={(e)=>setFormData({...formData,name:e.target.value})} className="w-full bg-red/5 border border-red/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-black"/>
                     </div>
