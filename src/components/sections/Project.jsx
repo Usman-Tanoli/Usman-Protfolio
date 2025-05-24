12ec6f0cf208a9fab0226aefa7c550dc4bf8455f
Your Name <you@example.com>
2025-05-24 13:22:10 +0500

    Add project

--- src/components/sections/Project.jsx
+++ src/components/sections/Project.jsx
@@ -25,8 +25,8 @@ export const Projects=()=>{
                         </div>
                     </div>
                     <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                        <h3 className="text-xl font-bold mb-2">Youtube</h3>
-                        <p className="text-gray-400 mb-4">Project description</p>
+                        <h3 className="text-xl font-bold mb-2">Task-Manager-App</h3>
+                        <p className="text-gray-400 mb-4">This application showcases proficiency in modern frontend development practices, emphasizing component-based architecture and efficient state management. It's an excellent demonstration of building a single-page application (SPA) with a focus on user experience and performance</p>
                         <div>
                         {['React','Nodejs','Appwrite','Tailwindcss'].map((tech,key)=>(
                             <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
@@ -34,7 +34,7 @@ export const Projects=()=>{
                         ))}
                          </div>
                         <div className="flex justify-between items-center">
-                        <a href="https:/www.youtube.com" className="text-blue-400 hover:text-blue-300 transition-colors my-4">
+                        <a href="https://Usman-Tanoli.github.io/task-manager-app" className="text-blue-400 hover:text-blue-300 transition-colors my-4">
                           View Projects ▶{''}
                         </a>
                         </div>
